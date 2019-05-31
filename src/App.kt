7fb8dedb6116808e52248f8c2af1806dbb5a3fc6
samy <ryio69007@gmail.com>
2019-05-31 23:29:10 +0200

    modify App.kt

--- src/App.kt
+++ src/App.kt
@@ -2,5 +2,9 @@ fun main() {
     println("hello!")
     var x=63
     var y : Long = x.toLong()
-    print(y)
+    println(y)
+
+    val myArray = arrayOf(1,2,3)
+    print("the first element of the array is ${myArray[0]} \n " +
+            "the size of myArray is ${myArray.size}")
 }
