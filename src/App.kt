510a88b4fb8a62187c0572844dfad262a163b51f
samy <ryio69007@gmail.com>
2019-06-01 15:58:43 +0200

    modify: src/App.kt

--- src/App.kt
+++ src/App.kt
@@ -1,10 +1,13 @@
 fun main() {
     println("hello!")
-    var x=63
+    var x = kotlin.random.Random.nextInt(10)
+        //(Math.random()* 10).toInt()  below kotlin 1.3
     var y : Long = x.toLong()
     println(y)
 
-    val myArray = arrayOf(1,2,3)
-    print("the first element of the array is ${myArray[0]} \n " +
-            "the size of myArray is ${myArray.size}")
+    val myArray = arrayOf(1,2,3,4,5)
+    val text = if (myArray.size > x)"greater than"
+     else if (myArray.size<x)"smaller than"
+    else "equal to"
+    println("my array is $text the value of x which is : $x ")
 }
